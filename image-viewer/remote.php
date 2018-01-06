7d7cfbbb1350c8fecc7ee4cceb83b44860a9fc6f
Dynamiter <filework@gmail.com>
2018-01-06 10:47:28 +0800

    第七題 圖片檢視工具 調整操作按鈕

--- image-viewer/remote.php
+++ image-viewer/remote.php
@@ -1,6 +1,12 @@
 <?php
 
-$files = glob("images/set_1/*.jpg");
+// 自行準備圖片集
+$image_set = [
+    'images/set_1/*.jpg',
+    'images/set_2/*.png'
+];
+
+$files = glob($image_set[1]);
 sort($files);
 
 header('Content-Type: application/json; charset=utf-8');
